705f4b9645c1f0c342e8752c1f9cca9e927e378c
Jakub Troszok <troszok@gmail.com>
2013-12-14 12:32:53 +0100

    Added friens route.

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Giftz::Application.routes.draw do
   get 'registration' => 'user#register', as: :registration
   devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   get 'friends' => 'welcome#friends', as: :friends
+  get 'friend/:id' => 'welcome#friend', as: :friend
 
   resources :gifts, only: [:index] do
     get :random, on: :collection
